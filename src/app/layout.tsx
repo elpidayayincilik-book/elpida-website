130cf0556f210f16d6c7dbdbd8f3f1f8ed9ea0e3
gokaydmrl <gokaydemirel96@gmail.com>
2025-07-08 15:16:04 +0300

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,9 @@ import ink from "@/assets/inkwhite.png";
 export const metadata: Metadata = {
   title: "Elpida Yayıncılık",
   description: "ELpida Yayınclık Websitesi",
+  icons: {
+    icon: "/elpida-logo.png",
+  },
 };
 
 export default function RootLayout({
