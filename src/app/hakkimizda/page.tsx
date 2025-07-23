5e721fb4a902d8bc81bfe28bd2875ef0b60f4729
gokaydmrl <gokaydemirel96@gmail.com>
2025-07-24 00:59:17 +0300

    fix

--- src/app/hakkimizda/page.tsx
+++ src/app/hakkimizda/page.tsx
@@ -1,17 +1,17 @@
 async function About() {
-  const about = await fetch("https://www.elpidakitap.com.tr/api/getAbout", {
-    cache: "force-cache",
-    next: {
-      revalidate: 36000,
-    },
-  });
+  // const about = await fetch("api/getAbout", {
+  //   cache: "force-cache",
+  //   next: {
+  //     revalidate: 36000,
+  //   },
+  // });
 
-  const data = await about.json();
+  // const data = await about.json();
 
   return (
     <div className="flex justify-center flex-col items-center p-4">
       <h1 className="font-bold text-xl font-serif ">HAKKIMIZDA</h1>
-      <p className="pt-4 whitespace-pre-line ">{data}</p>
+      <p className="pt-4 whitespace-pre-line ">{"data"}</p>
     </div>
   );
 }
