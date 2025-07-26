01a44b3004d61c8340a5357ed345ca0ca783dece
gokaydmrl <gokaydemirel96@gmail.com>
2025-07-26 12:51:13 +0300

    rm unused

--- src/app/api/getBooks/route.ts
+++ src/app/api/getBooks/route.ts
@@ -1,6 +1,6 @@
 import { supabase } from "../../../lib/supabase/server";
 export async function GET() {
-  const { data, error } = await supabase.from("books").select();
+  const { data } = await supabase.from("books").select();
   // console.log(error);
   console.log("getBooks worked");
 
