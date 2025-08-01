14f46f94d0777db9535af79c71c4c1a0849c0ae0
gokaydmrl <gokaydemirel96@gmail.com>
2025-08-02 01:13:23 +0300

    sitemap.ts added

---
+++ src/app/sitemap.ts
@@ -0,0 +1,39 @@
+import { getBooks } from "@/actions/getBooks";
+import type { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const books = await getBooks();
+  const booksMap: MetadataRoute.Sitemap = books!.map((item) => ({
+    url: `https://www.elpidakitap.com.tr/kitap/${item.url_slug}`,
+    lastModified: new Date(),
+    changeFrequency: "monthly",
+    priority: 1,
+  }));
+  return [
+    {
+      url: "https://www.elpidakitap.com.tr",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: "https://www.elpidakitap.com.tr/hakkimizda",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.elpidakitap.com.tr/yayin-paketlerimiz",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.elpidakitap.com.tr/iletisim",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    ...booksMap!,
+  ];
+}
