c3c5fbef961b343c9bfa775d8b641e39124db448
elpidayayincilik-book <elpidayayincilik@gmail.com>
2026-09-18 11:53:12 +0300

    Update page.tsx

--- src/app/yayin-paketlerimiz/page.tsx
+++ src/app/yayin-paketlerimiz/page.tsx
@@ -1,9 +1,10 @@
 import PublishDetails from "@/components/PublishDetails/PublishDetails";
 import { getPublishPackages } from "@/actions/actions";
-import { IPublishPackage } from "@/types/types";
-async function Publish() {
-  const data: IPublishPackage[] | undefined | null = await getPublishPackages();
 
-  return <PublishDetails publishDetails={data!} />;
+async function Publish() {
+  const data = await getPublishPackages();
+
+  return <PublishDetails publishDetails={data} />;
 }
+
 export default Publish;
