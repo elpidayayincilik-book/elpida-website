dbc568fd2b2f47aadd47224c5b2bde4252512ff6
elpidayayincilik-book <elpidayayincilik@gmail.com>
2026-09-18 09:55:06 +0300

    Update getBooks.ts

--- src/actions/getBooks.ts
+++ src/actions/getBooks.ts
@@ -1,6 +1,5 @@
 import { IBookWithAuthor } from "@/types/types";
 import { supabase } from "../lib/supabase/server";
-export const dynamic = "force-dynamic";
 export async function getBooks(): Promise<null | IBookWithAuthor[]> {
   const { data } = (await supabase
     .from("books")
